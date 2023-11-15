6753d5a99b79dda3ebcf2a8598e65ee660cc51ce
NehalNN10 <nh07884@st.habib.edu.pk>
2023-11-15 10:58:23 +0500

    comment

--- watergirl.cpp
+++ watergirl.cpp
@@ -1,6 +1,6 @@
 #include "watergirl.hpp"
 
-watergirl::watergirl() : character({779, 0, 294, 848},{0,0,50,50}){}
+watergirl::watergirl() : character({779, 0, 294, 848},{0,0,50,50}){} //default spawning location
 
 void watergirl::move() {
     moverRect.x += 2;
