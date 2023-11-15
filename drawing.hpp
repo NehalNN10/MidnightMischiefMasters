37b5f2fd58dcaba03037c8de8f8cf7e83c6f860e
NehalNN10 <nh07884@st.habib.edu.pk>
2023-11-15 11:23:50 +0500

    Borrowing drawing class

---
+++ drawing.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include<SDL.h>
+class Drawing{
+    public:
+        //The window renderer
+    static SDL_Renderer* gRenderer;
+    static SDL_Renderer* hRenderer;
+    //global reference to png image sheets
+    static SDL_Texture* assets;
+};
